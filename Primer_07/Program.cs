4e4994c2f89b6455bba22e72b595a6c8c0754225
UBaronRU <ubaron@mail.ru>
2023-01-22 01:17:52 +0300

    Primer_07 is down

---
+++ Primer_07/Program.cs
@@ -0,0 +1,13 @@
+﻿//## Primer_07
+//Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+//
+int [] array = new int[8];
+for (int i = 0; i < 8; i++)
+{
+    array[i] = new Random().Next(1,10);
+    if (array[i] < 6) 
+    {array[i] = 0;}
+    else
+    {array[i] = 1;}
+    Console.WriteLine("Элемент массива №" + i + " -> "+array[i]);
+}
