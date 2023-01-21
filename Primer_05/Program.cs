537080c6dd9cb6b135897b50c2b0c455201cdc76
UBaronRU <ubaron@mail.ru>
2023-01-22 00:05:38 +0300

    Primer_05 is down

---
+++ Primer_05/Program.cs
@@ -0,0 +1,11 @@
+﻿// Primer_05
+// Написать программу вычисления произведения чисел от 1 до N
+int Num1 = 0;
+int Num2 = 1;
+Console.Write("Введите число: ");
+Num1 = int.Parse(Console.ReadLine() ?? "0");
+for (int i = 1; i <= Num1; i++)
+{
+    Num2=Num2*i;
+}
+Console.WriteLine("Произведение чисел от 1 до "+ Num1 +" -> "+Num2);
