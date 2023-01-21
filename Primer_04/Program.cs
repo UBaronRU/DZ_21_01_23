f638881cd6239784d68bebc4d20042265581b040
UBaronRU <ubaron@mail.ru>
2023-01-21 23:44:11 +0300

    Primer_04 is down

---
+++ Primer_04/Program.cs
@@ -0,0 +1,33 @@
+﻿//Primer_04
+//Подсчитать сумму цифр в числе
+int Num1; // Основное число
+int Num3; // Основное число
+int Num2; // Основное число
+int Num_rzd; // Разрядность основного числа
+int Sum_rzd=0;
+Num_rzd = 1;
+string [] array = {"разрядов","разряд","разряда","разряда","разряда","разрядов","разрядов","разрядов","разрядов","разрядов",};
+string okonchanie= "";
+int temp_dig ;
+Console.Write("Введите число (не более 999999999) : ");
+Num1 = int.Parse(Console.ReadLine() ?? "0");
+// Определяем разрядность числа
+while (Num1 / Math.Pow(10,Num_rzd) > 1 & Num_rzd < 10)
+{
+Num_rzd = Num_rzd + 1; 
+}
+okonchanie = array [Num_rzd%10];
+Console.WriteLine(" У введенного числа "+Num1+" -> " + Num_rzd + " "+okonchanie);
+Num3=Num1;
+for (int i = Num_rzd; i > 0; i--)
+{
+    temp_dig = 1;
+    for (int ii = 1; ii < i; ii++)
+    {
+        temp_dig=temp_dig*10;
+    }
+    Num2 = Num3 / temp_dig;
+    Sum_rzd = Sum_rzd + Num2;
+    Num3 = Num3 - (Num2 * temp_dig);
+}
+    Console.WriteLine("Сумма разрядов веденного числа "+Num1+" = " + Sum_rzd);
