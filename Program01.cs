625c7e60ac061c8337c7d1c43ab1147f9072df36
UBaronRU <ubaron@mail.ru>
2023-01-21 17:14:54 +0300

    fix error in branch

--- Program01.cs
+++ Program01.cs
@@ -1,10 +1,1 @@
-﻿//Найти кубы чисел от 1 до N
-int Num1 = 0;
-int Num2 = 0;
-Console.Write("Введите число: ");
-Num1 = int.Parse(Console.ReadLine() ?? "0");
-for (int i = 1; i <= Num1; i++)
-{
-    Num2=i*i*i;
-    Console.WriteLine("Куб числа "+ i +" -> "+Num2);
-}
+﻿
