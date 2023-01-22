9737067d6901f8519817c4fc2dcb827fe067ca93
UBaronRU <ubaron@mail.ru>
2023-01-22 14:48:57 +0300

    fix Primer_07

--- Primer_07/Program.cs
+++ Primer_07/Program.cs
@@ -4,10 +4,6 @@
 int [] array = new int[8];
 for (int i = 0; i < 8; i++)
 {
-    array[i] = new Random().Next(1,10);
-    if (array[i] < 6) 
-    {array[i] = 0;}
-    else
-    {array[i] = 1;}
-    Console.WriteLine("Элемент массива №" + i + " -> "+array[i]);
+    array[i] = new Random().Next(2);
+        Console.WriteLine("Элемент массива №" + i + " -> "+array[i]);
 }
