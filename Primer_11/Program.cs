a7f011437d83ee1ad91d906d86c4cb8655150ac6
UBaronRU <ubaron@mail.ru>
2023-01-22 04:16:31 +0300

    Primer_11 is down

---
+++ Primer_11/Program.cs
@@ -0,0 +1,20 @@
+﻿// ## Primer_11
+// Найти сумму чисел одномерного массива стоящих на нечетной позиции
+int [] array = new int[new Random().Next(1,100)];
+int Len = 0;
+int Kol_c = 0;
+Len = array.Length;
+for (int i = 0; i < Len; i++)
+{
+    array[i] = new Random().Next(1,100);
+    Console.WriteLine("Элемент массива №" + i + " из "+ Len +" -> "+array[i]);
+}
+for (int i = 0; i < Len; i++)
+{
+        if (i%2 != 0)
+        {
+        Kol_c = Kol_c + array[i];
+        Console.WriteLine("Элемент массива №" + i + " найден  -> "+array[i]+" Сумма уже  уже "+Kol_c);
+        }
+}
+Console.WriteLine("Итого сумма чисел одномерного массива стоящих на нечетной позиции "+Kol_c);
